a4a1e86cbf39805d5e644d074e19708621637faa
Davi-Ga <davigaldinoky@gmail.com>
2022-10-06 01:17:18 -0300

    fragment do cabeçalho

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Cabecalho from '../src/components/Cabecalho/Cabecalho'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Cabecalho></Cabecalho>
+    <div>Conteudo</div>
+    </>
+  )
 }
 
 export default MyApp
