e974e3cfa7c12ffbfd02c5f1aebee48e893e2795
Davi-Ga <davigaldinoky@gmail.com>
2023-03-07 21:19:57 -0300

    ajustes

--- src/hooks/pages/useIndex.ts
+++ src/hooks/pages/useIndex.ts
@@ -1,4 +1,4 @@
-  import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Professor } from "../../@types/professor";
 import { ApiService } from "../../services/ApiService";
 
